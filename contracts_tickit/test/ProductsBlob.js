8b2ce072fbb4b7c0d0e373c207a61404071323f3
mindsweeper55 <mindsweeper55@gmail.com>
2018-10-13 21:00:04 +0200

    added test for getOfferCount and getOfferBy

--- contracts_tickit/test/ProductsBlob.js
+++ contracts_tickit/test/ProductsBlob.js
@@ -18,11 +18,37 @@ function(accounts) {
     it("GetOffersCount", function () {
         return productsBlob.deployed().then(function (instance) {
             productsInstance = instance;
-            return productsInstance.GetOffersCount();
-        }).then(function (retunValue) {
-            console.log(retunValue);
-            assert.equal(retunValue.c[0], 2);            
+            return productsInstance.GetOfferCount();
+        }).then(function (retunValue) {           
+            assert.equal(retunValue.c[0], 5);            
         });
     });
-    
+
+    it("GetOfferBy for offer 0", function () {
+        return productsBlob.deployed().then(function (instance) {
+            productsInstance = instance;
+            return productsInstance.GetOfferBy(0);
+        }).then(function (retunValue) {                   
+            assert.equal(retunValue[0].c[0], 0);  
+            assert.equal(retunValue[1].c[0], 0);     
+            assert.equal(retunValue[2], "0xabd49c5238abbf0b5f07ce6f357f3b871d33f045");     
+            assert.equal(retunValue[3], "RegioKarte");     
+            assert.equal(retunValue[4], "6.10.2018"); 
+            assert.equal(retunValue[5], "7.10.2018");
+        });
+    });
+
+    it("GetOfferBy for offer 1", function () {
+        return productsBlob.deployed().then(function (instance) {
+            productsInstance = instance;
+            return productsInstance.GetOfferBy(1);
+        }).then(function (retunValue) {                  
+            assert.equal(retunValue[0].c[0], 0);  
+            assert.equal(retunValue[1].c[0], 1);     
+            assert.equal(retunValue[2], "0xabd49c5238abbf0b5f07ce6f357f3b871d33f045");     
+            assert.equal(retunValue[3], "RegioKarte");
+            assert.equal(retunValue[4], "20.10.2018"); 
+            assert.equal(retunValue[5], "21.10.2018");   
+        });
+    }); 
 });
