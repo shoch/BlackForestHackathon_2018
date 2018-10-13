93a8bf475f1fed8a8ea714426affc0a77f34c5a5
Sebastian Hoch <sebi.hoch@web.de>
2018-10-13 11:18:46 +0200

    contracts skelleton
    empty contracts

---
+++ contracts_tickit/contracts/Offer.sol
@@ -0,0 +1,7 @@
+//solium-disable linebreak-style
+//solium-disable max-len
+
+pragma solidity ^0.4.4;
+
+contract Offer{
+}
---
+++ contracts_tickit/migrations/2_deploy_contracts.js
@@ -0,0 +1,12 @@
+var products = artifacts.require("./Products.sol");
+var product = artifacts.require("./Product.sol");
+var offer = artifacts.require("./offer.sol");
+var loan = artifacts.require("./loan.sol");
+
+
+module.exports = function(deployer) { 
+  deployer.deploy(products); 
+  deployer.deploy(product); 
+  deployer.deploy(offer); 
+  deployer.deploy(loan); 
+};
