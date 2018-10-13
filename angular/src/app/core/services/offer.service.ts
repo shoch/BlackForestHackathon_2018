9eed0f3f39dfe692268fd547483e944f01196715
mindsweeper55 <mindsweeper55@gmail.com>
2018-10-13 21:56:31 +0200

    added bcs to services

--- angular/src/app/core/services/offer.service.ts
+++ angular/src/app/core/services/offer.service.ts
@@ -4,22 +4,30 @@ import { Observable, of } from 'rxjs';
 import {OFFERS} from '../mock/offers';
 import {Offer} from '../models/offer.model';
 import {MessageService} from '../models/message.service';
+import {BCS} from './bcs';
 
 
 @Injectable({ providedIn: 'root' })
-export class OfferService {
+export class OfferService {  
 
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService) {       
+   }
 
-  getOffers(): Observable<Offer[]> {
+  async getOffers(): Observable<Offer[]> {
     // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('OfferService: fetched offers');
-    return of(OFFERS);
+     //this.messageService.add('OfferService: fetched offers');
+    //return of(OFFERS);
+    await BCS.init(); 
+    var offers = await BCS.GetOffers()   
+    return offers;
   }
 
-  getOffer(id: number): Observable<Offer> {
+  async getOffer(id: number): Observable<Offer> {
     // TODO: send the message _after_ fetching the hero
-    this.messageService.add(`OfferService: fetched offer id=${id}`);
-    return of(OFFERS.find(offer => offer.id === id));
+    //this.messageService.add(`OfferService: fetched offer id=${id}`);
+    //return of(OFFERS.find(offer => offer.id === id));
+    await BCS.init(); 
+    var offer = await BCS.GetOfferBy(id);
+    return offer;
   }
 }
--- angular/src/app/core/services/product.service.ts
+++ angular/src/app/core/services/product.service.ts
@@ -4,6 +4,7 @@ import { Observable, of } from 'rxjs';
 import {MessageService} from '../models/message.service';
 import {PRODUCTS} from '../mock/products';
 import {Product} from '../models/product.model';
+import {BCS} from './bcs';
 
 
 @Injectable({ providedIn: 'root' })
@@ -11,15 +12,21 @@ export class ProductService {
 
   constructor(private messageService: MessageService) { }
 
-  getProducts(): Observable<Product[]> {
+  async getProducts(): Observable<Product[]> {
     // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('ProductService: fetched offers');
-    return of(PRODUCTS);
+    //this.messageService.add('ProductService: fetched offers');
+    //return of(PRODUCTS);
+    await BCS.init(); 
+    var products = await BCS.GetProducts()   
+    return products;
   }
 
   getProduct(id: number): Observable<Product> {
     // TODO: send the message _after_ fetching the hero
-    this.messageService.add(`ProductService: fetched offer id=${id}`);
-    return of(PRODUCTS.find(product => product.id === id));
+    //this.messageService.add(`ProductService: fetched offer id=${id}`);
+    //return of(PRODUCTS.find(product => product.id === id));
+    await BCS.init(); 
+    var product = await BCS.GetProductBy(id)   
+    return product;
   }
 }
