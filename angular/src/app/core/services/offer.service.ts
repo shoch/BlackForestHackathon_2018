798cdae33dbb81d56a3793be88100d4ea83a4a3a
tim.gagliardi <iltis_dev@outlook.com>
2018-10-14 23:17:35 +0200

    adding missing changes

--- angular/src/app/core/services/offer.service.ts
+++ angular/src/app/core/services/offer.service.ts
@@ -25,4 +25,12 @@ export class OfferService {
     this.messageService.add(`OfferService: fetched offer id=${id}`);
     return of(OFFERS.find(offer => offer.id === id));
   }
+
+  searchOffer(productName: string, geolocation: string, start: string, end: string) {
+    if (!('offenburg'.includes(geolocation.toLocaleLowerCase())) || !('the hammer'.includes(productName.toLocaleLowerCase()))) {
+      return of([]);
+    }
+    //  of(OFFERS.find(offer => (Date.parse(offer.start) <= Date.parse(start))))&& (new Date(offer.end) >= new Date(end))
+    return of(OFFERS); //
+  }
 }
